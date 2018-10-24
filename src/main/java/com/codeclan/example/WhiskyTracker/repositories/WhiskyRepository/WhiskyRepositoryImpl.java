97af18fdb559e996c7ed84889661ff7b478ef588
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-24 21:44:14 +0100

    Initial commit. findAllWhiskiesForParticularYear created

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryImpl.java
@@ -0,0 +1,31 @@
+package com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class WhiskyRepositoryImpl implements WhiskyRepositoryCustom {
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Transactional
+    public List<Whisky> findAllWhiskiesForParticularYear(int age) {
+    List<Whisky> results = null;
+        Session session = entityManager.unwrap(Session.class);
+        Criteria cr = session.createCriteria(Whisky.class);
+        cr.add(Restrictions.gt("age", age));
+        results = cr.list();
+        return results;
+
+
+    }
+
+}
